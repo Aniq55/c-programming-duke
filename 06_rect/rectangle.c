360c5f1458d63f0c6cb3f196c71b6c86500a5e83
Coursera Learner <nobody@nowhere.nul>
2019-10-14 17:31:31 +0000

    06

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -42,7 +42,7 @@ rectangle intersection(rectangle r1, rectangle r2) {
   r3.width = min(r1.x + r1.width, r2.x + r2.width) - r3.x;
   r3.y = max(r1.y, r2.y);
   r3.height = min(r1.y + r1.height, r2.y + r2.height) - r3.y;
-  r3 = canonicalize(r3);
+  
   return r3;
 }
 
