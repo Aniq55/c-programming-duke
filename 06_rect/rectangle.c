dd2192d4886a6c0d98b12047375f436781adf8ae
Coursera Learner <nobody@nowhere.nul>
2019-10-14 17:23:25 +0000

    Did 06

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,15 +16,34 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+typedef struct rectangle{
+  int x;
+  int y;
+  int width;
+  int height;
+}rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if(r.width<0){
+    r.x = r.x + r.width;
+    r.width = -r.width;
+  }
+  if(r.height<0){
+    r.y = r.y + r.height;
+    r.height = -r.height;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  rectangle r3;
+  r3.x = max(r1.x, r2.x);
+  r3.width = min(r1.x + r1.width, r2.x + r2.width) - r3.x;
+  r3.y = max(r1.y, r2.y);
+  r3.height = min(r1.y + r1.height, r2.y + r2.height) - r3.y;
+  r3 = canonicalize(r3);
+  return r3;
 }
 
 //You should not need to modify any code below this line
