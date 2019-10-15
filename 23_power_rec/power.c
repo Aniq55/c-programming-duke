db6b7a9936c1e347795dab783a0d5ccc55109f2e
Coursera Learner <nobody@nowhere.nul>
2019-10-15 18:43:20 +0000

    corner cases for zero

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 unsigned int  power(unsigned int x, unsigned int y){
-  if(x==0)
-    return 1;
+  if(x==0){
+    if(y==0)
+      return 1;
+    return 0;
+  }
 
   if(y==0)
     return 1;
