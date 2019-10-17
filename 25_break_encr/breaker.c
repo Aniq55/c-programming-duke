7c339275d5d33fcf3a02f1902c7903345d13e880
Coursera Learner <nobody@nowhere.nul>
2019-10-17 18:12:03 +0000

    renamed file

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+  char * filename = argv[1];
+
+  FILE *fptr = fopen(filename, "r");
+
+  if (fptr == NULL){
+    printf("File %s not found", filename);
+    return EXIT_FAILURE;
+  }
+
+  char ch;
+
+  int arr[26] = {0};
+  while (ch != EOF){  
+    ch = fgetc(fptr);
+    arr[ch-'a']++;
+  }
+
+  fclose(fptr);
+
+  // find the character with highest frequency
+  int max_pos = 0;
+  for(int i=1; i<26; i++){
+    if(arr[i]> arr[max_pos])
+      max_pos = i;
+  }
+
+  int val =  max_pos + 'a' - 'e';
+
+  if(val<0)
+    val *= -1;
+
+  printf("%d\n", val);
+  
+  return 0;
+}
