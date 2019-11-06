6858ed0cd9a956d3b49341a77111eea67711a9cd
Coursera Learner <nobody@nowhere.nul>
2019-11-06 11:49:04 +0000

    done

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,59 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+char ** read_file(FILE *f, char **array, size_t *index){
+  char *line=NULL;
+  size_t sz=0;
+  while(getline(&line,&sz,f)>=0){
+    array=realloc(array,((*index)+1)*sizeof(*array));
+    array[*index]=malloc((strlen(line)+1)*sizeof(array[*index]));
+    strcpy(array[*index],line);
+    free(line);
+    line=NULL;
+    (*index)++;
+  }
+  free(line);
+  return array;
+}
+
+void print_and_free(char **array, size_t index){
+  size_t i=0;
+  while(i<index){
+    fprintf(stdout,"%s",array[i]);
+    free(array[i]);
+    i++;
+  }
+  free(array);
+}
+
 int main(int argc, char ** argv) {
-  
+
   //WRITE YOUR CODE HERE!
-  
+  size_t index=0;
+  char **array=NULL;
+  if(argc==1){
+    array=read_file(stdin,array,&index);
+    sortData(array,index);
+    print_and_free(array,index);
+    array=NULL;
+  }
+  else{
+    for(size_t i=1;i<argc;i++){
+      FILE *input=fopen(argv[i],"r");
+      if(input==NULL){
+	fprintf(stderr,"Can not open file '%s'\n",argv[i]);
+	return EXIT_FAILURE;
+      }
+      index=0;
+      array=read_file(input,array,&index);
+      sortData(array,index);
+      print_and_free(array,index);
+      array=NULL;
+      if(fclose(input)!=0){
+	fprintf(stderr,"Can not close file '%s'\n",argv[i]);
+	return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
